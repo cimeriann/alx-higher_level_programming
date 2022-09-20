09a1a9aff3e32d32ad181f37f70405eb835d00e9
Olayinka <adebisiolayinka30@gmail.com>
2022-09-20 15:36:35 +0000

    tasks uploaded

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,6 @@
+#!usr/bin/python
+def islower(c):
+    if ord(c) > 96:
+        return true
+    else:
+        return false
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+def uppercase(str):
+    for letter in str:
+        if ord(letter) > 65 and ord(letter) < 91:
+            print(letter, end="")
+        elif letter == " ":
+            print(letter, end="")
+        else:
+            print((chr(ord(letter)-32)), end="")

013f78ba43a4db5801e0b3ebba9e32f881e36d6e
Olayinka <adebisiolayinka30@gmail.com>
2022-09-22 08:47:30 -0700

    advanced tasks

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+
+def calculator():
+    from sys import argv
+    operators = ["+", "-", "*", "/"]
+    num_of_args = len(argv) - 1
+    if num_of_args == 3:
+        a = int(argv[1])
+        b = int(argv[3])
+        if argv[2] == "-":
+            result = int(argv[1]) - int(argv[3])
+            print("{:d} - {:d} = {:d}".format(a, b, result))
+            return 0
+        elif argv[2] == "+":
+            result = int(argv[1]) + int(argv[3])
+            print("{:d} + {:d} = {:d}".format(a, b, result))
+            return 0
+        elif argv[2] == "*":
+            result = int(argv[1]) * int(argv[3])
+            print("{:d} * {:d} = {:d}".format(a, b, result))
+            return 0
+        elif argv[2] == "/":
+            result = int(argv[1]) * int(argv[3])
+            print("{:d} / {:d} = {:d}".format(a, b, result))
+            return 0
+        elif argv[2] not in operators:
+            print("Unknown operator. Available operators: +, -, * and /")
+            return 1
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        return 1
+if __name__ == "__main__":
+    calculator()

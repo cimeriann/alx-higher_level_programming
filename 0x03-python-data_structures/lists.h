2606bb3a66dcd640e8badd8d738128f64b988cbb
Olayinka <adebisiolayinka30@gmail.com>
2022-09-25 11:40:01 -0700

    task 13 fix

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -18,4 +18,4 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
-#endif /* LISTS_
+#endif /* LISTS_H */

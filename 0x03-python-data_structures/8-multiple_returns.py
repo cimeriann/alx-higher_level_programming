191f8736a484ee2f8472eac7e6b40d1d61db3e0c
Olayinka <adebisiolayinka30@gmail.com>
2022-09-25 10:37:16 -0700

    task 3

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    first_chr = ""
+    if length == 0:
+        first_chr == None
+    first_chr = sentence[0]
+    return (length, first_chr)

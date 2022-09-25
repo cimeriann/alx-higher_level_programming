ab15bce76b660d80848008a1a69e0b16821d32c8
Olayinka <adebisiolayinka30@gmail.com>
2022-09-25 12:44:53 -0700

    made final changes

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
-
 def divisible_by_2(my_list=[]):
-    conclusion = []
-    for i in my_list:
+    conclusion = my_list[:]
+    for status, i in enumerate(my_list):
         if (i % 2) == 0:
-            conclusion.append(True)
+            conclusion[status] = True
         else:
-           conclusion.append(False)
-    return conclusion
+           conclusion[status] = False
+    return(conclusion)
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@
 def max_integer(my_list=[]):
     highest = 0
     if len(my_list) == 0:
-        return None
+        return ("None")
     for i in my_list:
         if i > highest:
             highest = i

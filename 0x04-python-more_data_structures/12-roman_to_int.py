af073f7c170b2537247bc24a45551278fd27ecaa
Olayinka <adebisiolayinka30@gmail.com>
2022-09-29 10:25:48 +0000

    task 12 fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    default_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    default_values = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
     total = 0
     next = 0
     if type(roman_string) != str or roman_string == None:
@@ -13,5 +13,5 @@ def roman_to_int(roman_string):
             else:
                 total += default_values[roman_string[i]]
         except IndexError:
-            total += default_values[[-1]]
+            total += default_values[roman_string[-1]]
     return total

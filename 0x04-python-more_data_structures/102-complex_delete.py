ad65c388fae0f7f45c4a15c8fbb3ef1620804289
Olayinka <adebisiolayinka30@gmail.com>
2022-09-29 10:56:31 +0000

    task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    targets = []
-    for key, key_value in my_dict.items():
-        if key_value is value:
-            targets.append(key)
-    for x in targets:
-        del my_dict[x]
-    return(my_dict)
+    def complex_delete(my_dict, value):
+    tmp_dict = my_dict.copy()
+    for k, v in my_dict.items():
+        if v == value:
+            del tmp_dict[k]
+    return tmp_dict
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,8 @@ def roman_to_int(roman_string):
     default_values = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
     total = 0
     next = 0
-    if type(roman_string) != str or roman_string == None:
-        return 0
+    if type(roman_string) != str or type(roman_string) == None:
+        return(0)
     for i in range(len(roman_string)):
         next = i+1
         try:

16b91fdfb7fde6816dba1d02d9b4347008045a08
Olayinka <adebisiolayinka30@gmail.com>
2022-09-29 10:58:40 +0000

    task 12 fix

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    def complex_delete(my_dict, value):
     tmp_dict = my_dict.copy()
     for k, v in my_dict.items():
         if v == value:

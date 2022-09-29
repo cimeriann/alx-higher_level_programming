a98f03f311ce0c26442a4133fb340eaddee7d603
Olayinka <adebisiolayinka30@gmail.com>
2022-09-29 11:05:53 +0000

    push task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,4 +4,5 @@ def complex_delete(my_dict, value):
     for k, v in my_dict.items():
         if v == value:
             del tmp_dict[k]
-    return tmp_dict
+    my_dict = tmp_dict.copy()
+    return my_dict

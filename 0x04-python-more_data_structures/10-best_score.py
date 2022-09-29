c4259231a5cb199fdfda54ae97a52f9b80971974
Olayinka <adebisiolayinka30@gmail.com>
2022-09-29 10:06:31 +0000

    fix files

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if my_dict is None or my_dict == {}:
+def best_score(my_dict):
+    highest_score = 0
+    name = ""
+    for key in my_dict:
+        if my_dict[key] > highest_score:
+            highest_score = my_dict[key]
+            name = key
+    if highest_score == 0:
         return None
-    biggest = max(my_dict.values())
-    for key, value in my_dict.items():
-        if value is biggest:
-            return key    
+    return name

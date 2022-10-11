906a898fa926014bc0d66278e7e7b4df8289c2f9
Olayinka <adebisiolayinka30@gmail.com>
2022-10-10 20:51:56 -0700

    advanced tasks

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,6 +11,7 @@ def list_division(my_list_1, my_list_2, list_length):
             res = 0
         except ZeroDivisionError:
             print("division by 0")
+            res = 0
         except IndexError:
             print("out of range")
             res = 0

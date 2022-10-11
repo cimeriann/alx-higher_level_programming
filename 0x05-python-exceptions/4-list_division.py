5734c3b6e5513705323c028eaf5ff7e3005978e8
Olayinka <adebisiolayinka30@gmail.com>
2022-10-11 04:04:00 +0000

    task 4 fix

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     ret = []
-    for i in range(list_length):
+    for i in range(0, list_length):
         try:
             res = my_list_1[i] / my_list_2[i]
         except TypeError:

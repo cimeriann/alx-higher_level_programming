bab3901663ee5823766e62173549e2844548a766
Olayinka <adebisiolayinka30@gmail.com>
2022-10-10 20:37:21 -0700

    mandatory tasks

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    res = 0
+    try:
+        res = a / b
+    except (ZeroDivisionError, TypeError):
+        res = None
+    finally:
+        print("Inside result: {}".format(res))
+    return (res)
---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    ret = []
+    for i in range(list_length):
+        try:
+            res = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            res = 0
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+            res = 0
+        finally:
+            ret.append(res)
+        return (ret)
